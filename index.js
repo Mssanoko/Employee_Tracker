ae0447cd37f23bcb6b525e2d32e19420a6081fdb
Leslie Lawson <lawsonjleslie@gmail.com>
2020-08-18 20:29:52 -0400

    connection to the sql database

--- index.js
+++ index.js
@@ -9,13 +9,5 @@ var connection = mysql.createConnection({
   database : 'employeet_db'
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log('')
-  });
 
-  connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-  });
    
