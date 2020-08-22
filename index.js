c851606706823ada20c36513cdf1385cb35a5475
Leslie Lawson <lawsonjleslie@gmail.com>
2020-08-22 18:06:10 -0400

    add employee

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ inquirer.prompt ([{
   }
   });
 }
+//View All Employees
 function employeesSearch() {
   connection.query(`SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
   FROM employee e LEFT JOIN role r ON e.role_id = r.id
@@ -76,5 +77,76 @@ function employeesSearch() {
     start();
   });
   }
-
+//Add Employees
+  function addEmployees() {
+    console.log("Inserting an employee!")
+  
+    var query =
+      `SELECT r.id, r.title, r.salary 
+        FROM role r`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      const roleChoices = res.map(({ id, title, salary }) => ({
+        value: id, title: `${title}`, salary: `${salary}`
+      }));
+  
+      console.table(res);
+      console.log("RoleToInsert!");
+  
+      promptInsert(roleChoices);
+    });
+  }
+  
+  function promptInsert(roleChoices) {
+  
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "What is the employee's first name?"
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "What is the employee's last name?"
+        },
+        {
+          type: "list",
+          name: "roleId",
+          message: "What is the employee's role?",
+          choices: roleChoices
+        },
+        // {
+        //   name: "manager_id",
+        //   type: "list",
+        //   message: "What is the employee's manager_id?",
+        //   choices: manager
+        // }
+      ])
+      .then(function (answer) {
+        console.log(answer);
+  
+        var query = `INSERT INTO employee SET ?`
+        // when finished prompting, insert a new item into the db with that info
+        connection.query(query,
+          {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            role_id: answer.roleId,
+            manager_id: answer.managerId,
+          },
+          function (err, res) {
+            if (err) throw err;
+  
+            console.table(res);
+            console.log(res.insertedRows + "Inserted successfully!\n");
+  
+            firstPrompt();
+          });
+        // console.log(query.sql);
+      });
+  }
   
